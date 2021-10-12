da5b3dec56068a99020bc40b1a231df133c83798
Watanabe Kazuki <watanabekazuki@Watanabes-MacBook-Pro.local>
2021-10-12 20:25:29 +0900

    textのリファクタリング

--- api/conf.py
+++ api/conf.py
@@ -5,10 +5,6 @@ import yaml
 class Texts(TypedDict):
     text: str
     question: str
-
-
-class Question(TypedDict):
-    text: str
     answer: Any
     nextText: str
 
@@ -16,7 +12,3 @@ class Question(TypedDict):
 def get_texts() -> Texts:
     with open('conf/text.yml') as text_file:
         return yaml.safe_load(text_file)
-
-def get_questions() -> Question:
-    with open('conf/question.yml') as question_file:
-        return yaml.safe_load(question_file)
--- api/main.py
+++ api/main.py
@@ -1,59 +1,31 @@
 import os
 from typing import Optional
-from fastapi import FastAPI, HTTPException, Body
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import PlainTextResponse
 from pydantic import BaseModel
-from api.conf import get_texts, get_questions
+from api.conf import get_texts
 
 ENDPOINT = os.getenv('ENDPOINT', '')
 PORT = os.getenv('PORT', 8001)
-# if not ENDPOINT:
-#     raise Exception('ENDPOINT environment is not defined')
 
 app = FastAPI(port=PORT)
 texts = get_texts()
-questions = get_questions()
 
 
-@app.get('/')
+@app.get('/', response_class=PlainTextResponse)
 def root():
-    return {
-        'message': f"""
+    return  f"""
 PyconJP 2021 JX通信社の脱出ゲームへようこそ！このゲームはHTTP Requestを用いて問題の出題・回答をします。
-ルールを表示する場合: /helpに対してGETリクエストをする
-ゲームを始める場合: {ENDPOINT}/startに対してGETリクエストをする
+ゲームを始める: /question?key=startに対してGETリクエストをする
 """
-    }
 
-@app.get('/help')
-def help_root():
-    return {
-        'message': f"""
-ここではルールの説明をします。このゲームはHTTP Requestを用いて問題の出題・回答をします。
-テキストのエンドポイント '{ENDPOINT}/text?key=start'を入れると最初の文章が出ます。その際のレスポンスには問題文と、回答先のエンドポイントに使うkeyも表示されます。
-問題文のエンドポイントは '{ENDPOINT}/question?key='です。keyに入れる内容は文章の'endpoint_key'にあるキーを利用してください。
-問題文はテキストのエンドポイントに記載されています。
-このエンドポイントに対してGETリクエストすることで問題文の表示ができ、POSTリクエストで回答ができます。POSTリクエストの際はRequest Bodyに'key' と、'answer'を入れてください。
-正解の場合はstatusCode: 200(OK)、間違っている場合はstatusCode: 400(Bad Request)が返ります。
-ゲームを始める場合: {ENDPOINT}/startに対してGETリクエストをする
-"""
-    }
 
-@app.get('/text')
+@app.get('/question', response_class=PlainTextResponse)
 def text_root(key: str = ''):
     if key and texts.get(key):
-        # textはmust、questionはないときもある
         text = texts[key]['text']
-        question = texts[key].get('question', '')
-        return {'text': text, 'endpoint_key': question}
-    return {'message': 'keyが設定されていません。keyはテキストのどこかに入っています。'}
-
-
-@app.get('/question')
-def question_get(key: str = ''):
-    if key and questions.get(key):
-        text = questions[key]['text']
-        return {'text': text}
-    return {'message': 'keyが設定されていません。keyはテキストのどこかに入っています。'}
+        return text
+    return 'keyが設定されていません。keyはテキストのどこかに入っています。'
 
 
 class QuestionAnswer(BaseModel):
@@ -69,9 +41,9 @@ def question_root(answer: QuestionAnswer):
     if answer.answer is None:
         raise HTTPException(status_code=400, detail='answerが入っていません。answerに対して回答を設定してリクエストしてください。')
 
-    if answer.key and questions.get(answer.key):
+    if answer.key and texts.get(answer.key):
         # strで比較する
-        if str(answer.answer) == str(questions[answer.key]['answer']):
-            return {'text': questions[answer.key]['nextText']}
+        if str(answer.answer) == str(texts[answer.key]['answer']):
+            return {'text': texts[answer.key]['nextText']}
         raise HTTPException(status_code=400, detail='正解ではありません。')
     raise {'message': 'keyが正しくありません'}
